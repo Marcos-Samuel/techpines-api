74dbb3d202ea771da4d2c08967c90d910d2c5c7e
Marcos-Samuel <marcos_samueloff@outlook.com>
2024-07-31 12:33:22 -0300

    teste de deploy

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,13 @@ use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\TrackController;
 use Illuminate\Support\Facades\Route;
 
-// Artistas
 Route::post('/artists', [ArtistController::class, 'addNewArtist']);
 Route::get('/artists/{id}', [ArtistController::class, 'listArtistWithAlbumsAndTracks']);
 
-// Álbuns
 Route::post('/albums', [AlbumController::class, 'addNewAlbum']);
 Route::get('/albums/{name}', [AlbumController::class, 'listAlbumByName']);
 Route::delete('/albums/{id}', [AlbumController::class, 'deleteAlbum']);
 
-// Faixas
 Route::post('/tracks', [TrackController::class, 'addNewTrack']);
 Route::get('/tracks/{name}', [TrackController::class, 'listTrackByName']);
 Route::delete('/tracks/{id}', [TrackController::class, 'deleteTrack']);
